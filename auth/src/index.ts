428722a6b1ee3ef1b50d8bd2d1f6d6f367e0dcda
Frank Gerold <FrankETSGerold@gmail.com>
2020-07-01 16:47:16 -0400

    Finished router skeleton for auth service. set up ingress, and changed dev script to poll  live skaffold updates.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,25 @@
 import express from 'express';
 import { json } from "body-parser";
 
+import { currentUserRouter } from "./routes/currentUser";
+import { signInRouter } from "./routes/signIn";
+import { signUpRouter } from "./routes/signUp";
+import { signOutRouter } from "./routes/signOut";
+
 
 const app = express();
 app.use(json());
 
+app.use(currentUserRouter);
+app.use(signUpRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+
+app.get('/api/users/hello', (req, res) => {
+  res.send('Hi there :)')
+});
+
 
 app.listen(1111, () => {
-  console.log('Auth listening on port 1111!');
-
+  console.log('Auth listening on port 1111!?');
 })
