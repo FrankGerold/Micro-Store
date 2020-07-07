b01985c6a0e97197e853df34bbe319c8acc2b00b
Frank Gerold <FrankETSGerold@gmail.com>
2020-07-07 11:51:22 -0400

    added error for unknown route requests

---
+++ auth/src/errors/notFoundError.ts
@@ -0,0 +1,19 @@
+import { CustomError } from "./customError";
+
+export class NotFoundError extends CustomError {
+  statusCode = 404;
+
+  constructor() {
+    super('Route Not Found!');
+
+    Object.setPrototypeOf(this, NotFoundError.prototype);
+  };
+
+  serializeErrors() {
+    return [
+      {
+        message: 'Not Found'
+      }
+    ];
+  };
+};
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,7 @@ import { signInRouter } from "./routes/signIn";
 import { signUpRouter } from "./routes/signUp";
 import { signOutRouter } from "./routes/signOut";
 import { errorHandler } from "./middleware/errorHandler";
+import { NotFoundError } from "./errors/notFoundError";
 
 
 const app = express();
@@ -15,6 +16,10 @@ app.use(currentUserRouter);
 app.use(signUpRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
