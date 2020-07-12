e9609b3827c84aa713260a3ddd01d6b4b0f56779
Frank Gerold <FrankETSGerold@gmail.com>
2020-07-12 19:18:05 -0400

    Tests for Signup. Changed secure connection requirement in cookie session logic for testing to work insecurely.

--- auth/src/routes/__test__/signUp.test.ts
+++ auth/src/routes/__test__/signUp.test.ts
@@ -11,3 +11,70 @@ it('returns a 201 on successful signup', async () => {
     })
     .expect(201);
 });
+
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: '123123.com',
+      password: '123123123'
+    })
+    .expect(400);
+
+});
+
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: '123@123.com',
+      password: 'pass'
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: '123@123.com' })
+    .expect(400);
+
+  return request(app)
+    .post('/api/users/signup')
+    .send({ password: '123123123'})
+    .expect(400);
+});
+
+
+it('doesn\'t allow duplicate emails', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: '123@123.com',
+      password: '123123123'
+    })
+    .expect(201);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: '123@123.com',
+      password: '123123123'
+    })
+    .expect(400);
+});
+
+
+it('sets a cookie after a successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: '123@123.com',
+      password: '123123123'
+    })
+    .expect(201);
+    console.log('Node Env:', process.env.NODE_ENV)
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
