adcefa936826b851ce59ab0f0da939a87c5aadfc
Frank Gerold <FrankETSGerold@gmail.com>
2020-07-12 10:47:45 -0400

    Refactor index into App.ts for actual server setup, index for local setup. Prep for testing.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,6 @@
-import express from 'express';
-import { json } from "body-parser";
-import "express-async-errors";
 import mongoose from "mongoose";
-import cookieSession from 'cookie-session';
 
-import { currentUserRouter } from "./routes/currentUser";
-import { signInRouter } from "./routes/signIn";
-import { signUpRouter } from "./routes/signUp";
-import { signOutRouter } from "./routes/signOut";
-import { errorHandler } from "./middleware/errorHandler";
-import { NotFoundError } from "./errors/notFoundError";
-
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true
-  })
-)
-
-app.use(currentUserRouter);
-app.use(signUpRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-
-app.all('*', () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
