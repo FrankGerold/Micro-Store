039c4d4aaada8ce75fc3967c20f3a89f451e20e7
Frank Gerold <FrankETSGerold@gmail.com>
2020-07-13 12:45:23 -0400

    Finish current user tests

--- auth/src/routes/__test__/currentUser.test.ts
+++ auth/src/routes/__test__/currentUser.test.ts
@@ -13,4 +13,13 @@ it('responds with details about current user', async () => {
     .expect(200)
 
   expect(response.body.currentUser.email).toEqual('123@123.com')
-})
+});
+
+it('responds with null if not authenticated', async () => {
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200)
+
+  expect(response.body.currentUser).toEqual(null);
+});
