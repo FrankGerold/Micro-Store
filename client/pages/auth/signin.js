b4b58bfd931aa3435148eddb575c813aac27d1ae
Frank Gerold <FrankETSGerold@gmail.com>
2020-07-14 21:02:33 -0400

    Sign in page. Copy pasted signup, need to refactor both to be DRY

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import Router from 'next/router';
+
+import useRequest from '../../hooks/useRequest'
+
+export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [signInRequest, formErrors] = useRequest({
+    url: '/api/users/signin',
+    method: 'post',
+    body: {
+      email,
+      password
+    },
+    onSuccess: () => Router.push('/')
+  });
+
+  const onSubmit = async event => {
+    event.preventDefault()
+
+    signInRequest();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign In</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input className="form-control" value={email} onChange={e => setEmail(e.target.value)} />
+      </div>
+
+      <div className="form-group">
+        <label>Password</label>
+        <input type="password" className="form-control" value={password} onChange={e => setPassword(e.target.value)} />
+      </div>
+
+      {formErrors}
+
+      <button className="btn btn-primary">Sign In</button>
+    </form>
+  );
+};
