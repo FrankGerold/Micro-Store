4e3bf8bc3f01978c33aa8f28c78a03455776ab9d
Frank Gerold <FrankETSGerold@gmail.com>
2020-08-25 13:59:07 -0400

    Update works

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import { app } from '../../app';
 import { getAuthCookie } from '../../test/getAuthCookie';
 import { Ticket } from '../../models/ticket'
+import { validateRequest } from '@microstore/common';
 
 it('returns a 404 if the provided id doesn\'t exist', async () => {
 	const id = new mongoose.Types.ObjectId().toHexString();
@@ -63,12 +64,80 @@ it('returns a 401 if the user doesn\'t own the ticket', async () => {
 
 
 it('returns a 400 if the provides an invalid title', async () => {
-	
+	const cookie = getAuthCookie();
+
+	const response = await request(app)
+		.post('/api/tickets')
+		.set('Cookie', cookie)
+		.send({
+			title: 'New Test Ticket',
+			price: 420
+		})
+		.expect(201);
+
+	await request(app)
+		.put(`/api/tickets/${response.body.id}`)
+		.set('Cookie', cookie)
+		.send({
+			title: '',
+			price: 420
+		})
+		.expect(400)
 });
 
 
 it('returns a 400 if the provides an invalid price', async () => {
-	
+	const cookie = getAuthCookie();
+
+	const response = await request(app)
+		.post('/api/tickets')
+		.set('Cookie', cookie)
+		.send({
+			title: 'New Test Ticket',
+			price: 420
+		})
+		.expect(201);
+
+	await request(app)
+		.put(`/api/tickets/${response.body.id}`)
+		.set('Cookie', cookie)
+		.send({
+			title: 'New Test Ticket',
+			price: -1
+		})
+		.expect(400);
+});
+
+
+it('updates the ticket when given valid inputs', async () => {
+	const cookie = getAuthCookie();
+
+	const response = await request(app)
+		.post('/api/tickets')
+		.set('Cookie', cookie)
+		.send({
+			title: 'New Test Ticket',
+			price: 420
+		})
+		.expect(201);
+
+	const ticketId = response.body.id;
+
+	const updatedTicket = {
+		title: 'Updated ticket!',
+		price: 4321
+	}
+	await request(app)
+		.put(`/api/tickets/${ticketId}`)
+		.set('Cookie', cookie)
+		.send(updatedTicket)
+		.expect(200);
+
+	const ticketInfo = await Ticket.findById(ticketId);
+
+	expect(ticketInfo?.title).toEqual(updatedTicket.title);
+	expect(ticketInfo?.price).toEqual(updatedTicket.price);
+
 });
 
 
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -11,7 +11,16 @@ import {
 
  const router = express.Router();
 
- router.put('/api/tickets/:id', requireAuth, async (req: Request, res: Response) => {
+ router.put('/api/tickets/:id', requireAuth, [
+	body('title')
+		 .not()
+		 .isEmpty()
+		 .withMessage('Title is required!'),
+	body('price')
+		.isFloat({ gt: 0 })
+		.withMessage('Price must be greater than 0!')
+ ],
+  validateRequest, async (req: Request, res: Response) => {
 	 const ticket = await Ticket.findById(req.params.id);
 
 	 if (!ticket) {
@@ -21,6 +30,13 @@ import {
 	 if (ticket.userId !== req.currentUser!.id) {
 		 throw new NotAuthorizedError();
 	 };
+
+	 ticket.set({
+		 title: req.body.title,
+		 price: req.body.price
+	 });
+
+	 await ticket.save();
 
 	 res.send(ticket);
  });
