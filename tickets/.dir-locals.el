29efe9eb68565cd525328dd27c5ecada9f3aee78
Frank Gerold <FrankETSGerold@gmail.com>
2020-09-10 18:00:09 -0400

    Added linter stuff to tickets for emacs integration

---
+++ tickets/.dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((javascript-mode . ((node-add-modules-path . t))))
--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -4,17 +4,17 @@ interface TicketAttrs {
   title: string;
   price: number;
   userId: string;
-};
+}
 
 interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
   userId: string;
-};
+}
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
     build(attrs: TicketAttrs): TicketDoc;
-};
+}
 
 const ticketSchema = new mongoose.Schema({
   title: {
