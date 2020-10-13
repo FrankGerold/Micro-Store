8f7f68518445773dd793af1a61606c5b9ca87ce4
Frank Gerold <FrankETSGerold@gmail.com>
2020-10-13 19:43:08 -0400

    Created skeleton of Order service based on Tickets. Set up k8s for it. Also added some stuff to gitignores and dockerignores to hopefully stop emacs from messing with skaffold so much.

---
+++ orders/src/index.ts
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+import { app } from "./app";
+import { natsWrapper } from './natsWrapper';
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT key env var must be defined!')
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error('Mongo URI env var must be defined!')
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS URL env var must be defined!')
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS Cluster ID env var must be defined!')
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS Client ID env var must be defined!')
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
+    await mongoose.connect(
+      process.env.MONGO_URI,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+      }
+    );
+
+    console.log('Connected to MongoDB!');
+
+  } catch (err) {
+    console.error(err);
+  };
+};
+
+
+app.listen(2222, () => {
+  console.log('Tickets listening on port 2222');
+})
+
+
+start();
