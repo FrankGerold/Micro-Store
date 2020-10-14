daa5973c7bfc6396aa5a64f7184c610022d7ce0e
Frank Gerold <FrankETSGerold@gmail.com>
2020-10-14 19:31:45 -0400

    New Order checks for ticket and preexisting order for validation.

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -2,7 +2,9 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import mongoose from 'mongoose';
 
-import { requireAuth, validateRequest } from '@microstore/common';
+import { requireAuth, validateRequest, NotFoundError, OrderStatus, BadRequestError } from '@microstore/common';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -13,6 +15,40 @@ router.post('/api/orders', requireAuth, [
     .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
     .withMessage('Must provide MongoDB Ticket ID!')
 ], validateRequest, async (req: Request, res: Response) => {
+  const { ticketId } = req.body;
+
+  // Find the ticket in the DB
+  const ticket = await Ticket.findById(ticketId);
+
+  if (!ticket) {
+    throw new NotFoundError();
+  };
+
+  // Make sure ticket isnt already reserved
+  // Query thru all orders, find one with ticket just found, and status is NOT cancelled.
+  const existingOrder = await Order.findOne({
+    ticket: ticket,
+    status: {
+      $in: [
+        OrderStatus.AwaitingPayment,
+        OrderStatus.Complete,
+        OrderStatus.Created
+      ]
+    }
+  });
+
+  if (existingOrder) {
+    throw new BadRequestError('Ticket already reserved!');
+  };
+
+  // Calculate order expiration
+
+
+  // Build order, save to DB
+
+
+  // Publish event
+
   res.send({});
 });
 
