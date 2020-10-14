640328e577565f702f748fe2a533c0fbfccaadb6
Frank Gerold <FrankETSGerold@gmail.com>
2020-10-13 20:49:49 -0400

    Add validation to new order route, including custom ID verification

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,18 @@
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
+
+import { requireAuth, validateRequest } from '@microstore/common';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
+router.post('/api/orders', requireAuth, [
+  body('ticketId')
+    .not()
+    .isEmpty()
+    .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+    .withMessage('Must provide MongoDB Ticket ID!')
+], validateRequest, async (req: Request, res: Response) => {
   res.send({});
 });
 
